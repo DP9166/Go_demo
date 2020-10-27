373751cccee06d240a8d9db020627626abba91a6
DP9166 <149022511@qq.com>
2020-10-27 18:03:12 +0800

    备份

---
+++ Object/demo.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func printError()  {
+	fmt.Println("兜底执行")
+}
+
+func divide()  {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("Runtime panic caught: %v\n", err)
+		}
+	}()
+
+	var i = 1
+	var j = 1
+	k := i / j
+	fmt.Printf("%d / %d = %d\n", i, j, k)
+}
+
+func main() {
+
+	divide()
+	fmt.Println("divide方法调用完毕,回到main函数")
+	//defer printError()
+	//defer func() {
+	//	fmt.Println("除数不能是0!")
+	//}()
+
+	//defer func() {
+	//	fmt.Print("代码清理逻辑")
+	//}()
+	//
+	//var i = 1
+	//var j = 0
+	//if j == 0 {
+	//	panic("除数不能为0！")
+	//}
+	//
+	//k := i / j
+	//fmt.Printf("%d / %d = %d\n", i, j, k)
+}
--- demo.go
+++ demo.go
@@ -1,16 +1,32 @@
 package main
 
-import (
-	"calc/src/animal"
-	"fmt"
-)
+import "fmt"
+
+type A interface {
+	Foo()
+}
+
+type B interface {
+	A
+	Bar()
+}
+
+type T struct {}
+
+func (t T) Foo() {
+	fmt.Println("call Foo function from interface A.")
+}
+
+func (t T) Bar() {
+	fmt.Println("call Bar function from interface B.")
+}
 
 func main() {
-	var animal = animal.NewAnimal("小狗")
-
-	fmt.Println(animal.GetName())
-	//var ianimal oop.IAnimal = oop.Dog{animal}
+	//var animal = oop.NewAnimal("小狗")
+	//var ianimal oop.IAnimal = oop.Dog{*animal}
+	//fmt.Println(reflect.TypeOf(ianimal))
 	//if dog, ok := ianimal.(oop.Dog); ok {
 	//	fmt.Println(dog.GetName())
 	//}
+	//fmt.Println(reflect.TypeOf(ianimal))
 }
